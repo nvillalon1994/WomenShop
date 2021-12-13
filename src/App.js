93cac23d78d547929647ad5242c978e0d7be88f4
nvillalon1994 <70975528+nvillalon1994@users.noreply.github.com>
2021-12-13 02:34:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import ManShop from './ManShop'
+import WomanShop from'./WomanShop'
+import Home from './Home'
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Routes>
+        <Route path ="/aplicaicon" element={<Home/>}></Route>
+        <Route path="/ManShop.js" element={<ManShop/>}/>
+        <Route path="/WomanShop.js" element={<WomanShop/>}/>
+        
+
+      </Routes> */}
+      <WomanShop></WomanShop>
+      
+     
+      
     </div>
-  );
+  )
 }
 
 export default App;
